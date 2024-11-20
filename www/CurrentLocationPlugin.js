6cbbe8337dab54fedfea56c874ba1a167147d666
Deep Thakre <thakaredeep43@gmail.com>
2024-11-20 19:29:44 +0530

    second commit

--- www/CurrentLocationPlugin.js
+++ www/CurrentLocationPlugin.js
@@ -1,5 +1,10 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'CurrentLocationPlugin', 'coolMethod', [arg0]);
+/**
+ * Get the current location of the user.
+ * @param {function} success - Callback for success with location data.
+ * @param {function} error - Callback for handling errors.
+ */
+exports.getCurrentLocation = function (success, error) {
+    exec(success, error, 'CurrentLocationPlugin', 'getCurrentLocation', []);
 };
